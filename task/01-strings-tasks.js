aa262778f20368da9c1c8070ab75f5d87efafc7e
DadiaDadushko <dariadadushko@gmail.com>
2021-04-06 15:28:21 +0300

    convertToUpperCase

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -155,7 +155,7 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-  throw new Error("Not implemented");
+  return str.toUpperCase();
 }
 
 /**
