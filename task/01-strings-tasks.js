469d6e6c0f1ef9c59e5abbb6c15db5b7c7bee6b3
DadiaDadushko <dariadadushko@gmail.com>
2021-04-08 10:39:49 +0300

    removeLeadingAndTrailingWhitespaces

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -95,7 +95,7 @@ function getFirstChar(value) {
  *   '\tHello, World! ' => 'Hello, World!'
  */
 function removeLeadingAndTrailingWhitespaces(value) {
-  throw new Error("Not implemented");
+  return value.trim();
 }
 
 /**
